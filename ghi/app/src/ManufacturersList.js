d542ce90b8ff12192b50086dc9c49bfdf2a73842
Junella Caringal <junellacaringal@gmail.com>
2022-06-20 13:38:20 -0700

    Added title to Manufacturer list page

--- ghi/app/src/ManufacturersList.js
+++ ghi/app/src/ManufacturersList.js
@@ -4,21 +4,24 @@ export default function ManufacturerList(props) {
   const { manufacturers } = props;
 
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {manufacturers.map((manufacturer) => {
-          return (
-            <tr key={manufacturer.id}>
-              <td>{manufacturer.name}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <h2>Manufacturers</h2>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {manufacturers.map((manufacturer) => {
+            return (
+              <tr key={manufacturer.id}>
+                <td>{manufacturer.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }

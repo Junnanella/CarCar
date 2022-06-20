998de8255af29bef30a335a4c9016ae49966478b
Junella Caringal <junellacaringal@gmail.com>
2022-06-20 12:33:19 -0700

    Imported Inventory component to App.js and set up routes. Working.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+
+// import Inventory components
+import ManufacturerList from "./ManufacturersList";
+import ManufacturerForm from "./ManufacturerForm";
+import VehicleList from "./VehiclesList";
+import VehicleForm from "./VehicleForm";
+import AutomobileList from "./AutomobileList";
+import AutomobileForm from "./AutomobileForm";
+
+// import Services components
+
+// import Sales components
 
 function App() {
   return (
@@ -9,6 +21,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturers/" element={<ManufacturerList />} />
+          <Route path="manufacturers/new/" element={<ManufacturerForm />} />
+          <Route path="vehicles/" element={<VehicleList />} />
+          <Route path="vehicles/new/" element={<VehicleForm />} />
+          <Route path="automobiles/" element={<AutomobileList />} />
+          <Route path="automobiles/new/" element={<AutomobileForm />} />
         </Routes>
       </div>
     </BrowserRouter>

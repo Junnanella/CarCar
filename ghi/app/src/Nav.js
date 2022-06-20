75c17a21b463f80092cd71ed400f469bcb35be07
Junella Caringal <junellacaringal@gmail.com>
2022-06-20 12:33:43 -0700

    Added links to components in Nav.js. Working.

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,20 +1,75 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <NavLink className="navbar-brand" to="/">
+          CarCar
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <NavLink className="nav-link active" aria-current="page" to="/">
+              Home
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="manufacturers/"
+            >
+              Manufacturers
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="manufacturers/new/"
+            >
+              Add Manufacturer
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="vehicles/"
+            >
+              Vehicle Models
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="vehicles/new/"
+            >
+              Add Vehicle Model
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="automobiles/"
+            >
+              Automobiles
+            </NavLink>
+            <NavLink
+              className="nav-link active"
+              aria-current="page"
+              to="automobiles/new/"
+            >
+              Add Automobile
+            </NavLink>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav;

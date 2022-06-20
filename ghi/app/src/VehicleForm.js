45e88ad449634193c3d91025459d84e8b3538e68
Junella Caringal <junellacaringal@gmail.com>
2022-06-20 12:32:16 -0700

    Added components for Vehicle  List and Form. In progress, but working.

---
+++ ghi/app/src/VehicleForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default class VehicleForm extends React.Component {
+  render() {
+    return <h1>Vehicle Form Testing</h1>;
+  }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         name: "",
+  //     }
+  //     bind
+  // };
+
+  // wait for component to mount for manufacturers
+  // async componentDidMount() {
+  // url for api data
+
+  // await fetch
+
+  // if response.ok
+  // set data to response.json
+  // setState for manufacturers
+  // }
+
+  // handleSubmit
+
+  // handle changes on form
+
+  // render() {
+  // return(
+
+  // )
+  // }
+}
---
+++ ghi/app/src/VehiclesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function VehicleList(props) {
+  // const {vehicle} = props;
+
+  return <h1>Vehicles List Testing</h1>;
+}

8209c9cc40216423253d40250bf6cf82e57e615b
Junella Caringal <junellacaringal@gmail.com>
2022-06-20 12:32:42 -0700

    Added components for Automobile List and Form. In progress, but working.

---
+++ ghi/app/src/AutomobileForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default class AutomobileForm extends React.Component {
+  render() {
+    return <h1>Automobile Form Testing</h1>;
+  }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         name: "",
+  //     }
+  //     bind
+  // };
+
+  // wait for component to mount for manufacturers
+  // async componentDidMount() {
+  // url for api data
+
+  // await fetch
+
+  // if response.ok
+  // set data to response.json
+  // setState for manufacturers
+  // }
+
+  // handleSubmit
+
+  // handle changes on form
+
+  // render() {
+  // return(
+
+  // )
+  // }
+}
---
+++ ghi/app/src/AutomobileList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AutomobileList(props) {
+  // const {manufacturer} = props;
+
+  return <h1>Automobile List Testing</h1>;
+}

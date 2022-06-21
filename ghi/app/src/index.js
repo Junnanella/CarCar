aff2a41aa709516bbb77032d71b8b35340191528
Junella Caringal <junellacaringal@gmail.com>
2022-06-21 15:49:07 -0700

    Moved fetch functions for inventory to inventory components directory.

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -2,25 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Load Data functions
-const loadManufacturers = async () => {
-  // fetch data from api
-  const response = await fetch("http://localhost:8100/api/manufacturers/");
-  const responseJson = await response.json();
-  return responseJson;
-};
-
-const loadVehicles = async () => {
-  const response = await fetch("http://localhost:8100/api/models/");
-  const responseJson = await response.json();
-  return responseJson;
-};
-
-const loadAutomobiles = async () => {
-  const response = await fetch("http://localhost:8100/api/automobiles/");
-  const responseJson = await response.json();
-  return responseJson;
-};
+import { loadManufacturers } from "./inventory_components/InventoryApi";
+import { loadVehicles } from "./inventory_components/InventoryApi";
+import { loadAutomobiles } from "./inventory_components/InventoryApi";
 
 // invoke loading functions through main so they are all in one place
 const main = async () => {

24b8f612e240aae3d46ca0c70f20542d5efcb5b2
Junella Caringal <junellacaringal@gmail.com>
2022-06-22 12:12:27 -0700

    Added comments for fetching data for service components.

---
+++ ghi/app/src/services_components/ServicesAPI.js
@@ -0,0 +1,19 @@
+//"🐰🐰🐰" Comment out fetching functions below
+
+// export const loadServiceAppointments = async () => {
+//     const response = await fetch("http://localhost:8080/api/appointments");
+//     const responseJson = await response.json();
+//     return responseJson.service;
+// }
+
+// export const loadTechnicians = async () => {
+//     const response = await fetch("http://localhost:8080/api/technicians");
+//     const responseJson = await response.json();
+//     return responseJson.technicians;
+// }
+
+// export const loadAutomobiles = async () => {
+//     const response = await fetch("http://localhost:8100/api/automobiles/");
+//     const responseJson = await response.json();
+//     return response.automobiles;
+// }

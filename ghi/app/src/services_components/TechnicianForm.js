2f9d57d76625543fe41a411a05b7a6941a20014d
Junella Caringal <junellacaringal@gmail.com>
2022-06-22 11:16:13 -0700

    Able to handle state changes in technician form on browser.

--- ghi/app/src/services_components/TechnicianForm.js
+++ ghi/app/src/services_components/TechnicianForm.js
@@ -2,8 +2,63 @@ import React, { useState } from "react";
 
 export const CreateTechnicianForm = (props) => {
   // declaring variables and setting initial states
-  //   const [name, setName] = useState("");
-  //   const [employee_number, setEmployeeNumber] = useState("");
+  const [values, setValues] = useState({
+    name: "",
+    employee_number: "",
+  });
 
-  return <h1>Testing Technician Form</h1>;
+  const handleChangeName = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      name: event.target.value,
+    }));
+  };
+
+  const handleChangeEmployeeNumber = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      employee_number: event.target.value,
+    }));
+  };
+
+  return (
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Add a Technician</h1>
+          <form>
+            <div className="form-floating mb-3">
+              <input
+                onChange={handleChangeName}
+                value={values.name}
+                placeholder="Technician Name"
+                required
+                type="text"
+                name="name"
+                id="name"
+                className="form-control"
+              />
+              <label htmlFor="name">Technician Name</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                onChange={handleChangeEmployeeNumber}
+                value={values.employee_number}
+                placeholder="Employee Number"
+                required
+                type="text"
+                name="employee_number"
+                id="employee_number"
+                className="form-control"
+              />
+              <label htmlFor="employee_number">Employee Number</label>
+            </div>
+            <button className="btn btn-primary">Add</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };

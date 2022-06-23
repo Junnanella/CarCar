6393da7e236cace0e4141cae700544e171399108
Junella Caringal <junellacaringal@gmail.com>
2022-06-23 16:21:42 -0700

    Updated imports and routes. Working.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -15,7 +15,8 @@ import { SalesPersonForm } from "./sales_components/SalesPersonForm";
 import CreateSalesRecord from "./sales_components/SalesRecordForm";
 import { CreateTechnicianForm } from "./services_components/TechnicianForm";
 import CreateAppointmentForm from "./services_components/AppointmentsForm";
-import ServiceAppointmentList from "./services_components/AppointmentList";
+import ServiceAppointmentList from "./services_components/AppointmentsList";
+import { CustomerForm } from "./sales_components/CustomerForm";
 
 // 🚨🚨🚨 Import sales history class or function🚨🚨🚨
 // 🚨🚨🚨 Then add route in the return of App function🚨🚨🚨
@@ -48,8 +49,14 @@ function App(props) {
           <Route path="customer/new/" element={<CustomerForm />} />
           <Route path="sales_record/new/" element={<CreateSalesRecord />} />
           <Route path="technicians/new/" element={<CreateTechnicianForm />} />
-          <Route path="service_appointments/new/" element={<CreateAppointmentForm />} />
-          <Route path="service_appointments/" element={<ServiceAppointmentList />} />
+          <Route
+            path="service_appointments/new/"
+            element={<CreateAppointmentForm />}
+          />
+          <Route
+            path="service_appointments/"
+            element={<ServiceAppointmentList />}
+          />
         </Routes>
       </div>
     </BrowserRouter>

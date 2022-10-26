2a2ecbc3f385d11c48976270fcd6020983cb0a91
Junella Caringal <junellacaringal@gmail.com>
2022-10-25 21:38:52 -0700

    added image to main page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,7 +1,11 @@
+import "./index.css";
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold">CarCar</h1>
+      <img className="mainpageImage" src="/homePageImage.png" alt="CarCar image" />
+      {/* Image by macrovector on Freepik */}
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
           The premiere solution for automobile dealership
